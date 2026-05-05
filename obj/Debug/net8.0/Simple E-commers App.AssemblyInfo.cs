0311a8ae623c8309bcf7925f6197163d39e278de
Abdalla Ahmed <abdalaahmed2009452@gmail.com>
2026-05-05 17:12:37 +0300

    chore: add .gitignore and clean git cache

--- obj/Debug/net8.0/Simple E-commers App.AssemblyInfo.cs
+++ obj/Debug/net8.0/Simple E-commers App.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Simple E-commers App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82b82a341dab801dee2437e96cc193b27a1947a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c51e20792b5e116b79e6342161ce291729bcbdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Simple E-commers App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Simple E-commers App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
